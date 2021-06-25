a5b226a9d903ae12da0782822f288ba71ebd4e32
sumaiyadabeer <45242496+sumaiyadabeer@users.noreply.github.com>
2021-06-25 12:42:37 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,364 @@
+#include <iostream>
+#include "sort.h"
+#include <mpi.h>
+#include <cstdlib>
+using namespace std;
+//********************QUICK SORT*******************//
+void pSort::swap(dataType *data1, dataType *data2)
+{ 
+     pSort::dataType *t = new pSort::dataType[1];
+     *t = *data1; 
+     *data1 = *data2; 
+     *data2 = *t; 
+} 
+
+int pSort::partition (dataType *data, int low, int high) 
+{ 
+    int pivot,i;
+    i = (low - 1); 
+    pivot = (data+high)->key; 
+ 
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        
+        if ((data+j)->key <= pivot) 
+        { 
+            i++;    
+            swap((data+i), (data+j)); 
+        } 
+    } 
+    swap((data+i+1), (data+high)); 
+    return (i + 1); 
+} 
+
+void pSort::quick_sort(dataType *data, int low, int high) 
+{ 
+// arr= 1 2 5 3 7 10 2 1 
+
+
+    if (low < high) 
+    {        
+        int part = partition(data, low, high); 
+        quick_sort(data, low, part - 1); 
+        quick_sort(data, part + 1, high); 
+    } 
+} 
+/*********************RADIX SORT***********************/
+int pSort::getMaximum(dataType *data, int low, int high) 
+{ 
+    int mx = (data+low)->key; 
+    for (int i = low; i < high; i++) 
+        if ((data+i)->key > mx) 
+            mx = (data+i)->key; 
+    return mx; 
+} 
+
+void pSort::unit_sort(dataType *data, int low, int high, int exp) 
+{ 
+   
+    pSort::dataType *output = new pSort::dataType[high-low];
+    int i, count[10] = { 0 }; 
+  
+    
+    for (i = low; i < high; i++) 
+        count[(abs((data+i)->key)  / exp) % 10]++; 
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; 
+  
+    for (i = high-1; i >= low; i--) { 
+        (output+(count[(abs((data+i)->key) / exp) % 10] - 1))->key = (data+i)->key; 
+
+        *(output+(count[abs(((data+i)->key) / exp) % 10] - 1))->payload = *(data+i)->payload; 
+        count[(abs((data+i)->key) / exp) % 10]--; 
+    } 
+  
+    count[0]=0;
+    for (i = low; i < high; i++) {
+        (data+i)->key = (output+count[0])->key;
+        *(data+i)->payload = *(output+count[0])->payload; 
+        count[0]++;
+    }
+}
+
+void pSort::radix_sort(dataType *data, int low, int high)
+{
+   int m = getMaximum(data, low,high); 
+   int i;   
+    for ( i = 1; m / i > 0; i *= 10) 
+        unit_sort(data, low,high, i); 
+    pSort::dataType *TEMP = new pSort::dataType[high]; 
+ m=0;
+    for(i = high-1; i >=0; i--){
+	if((data+i)->key<0){
+        *(TEMP+m) = *(data+i);
+	m++;
+		}
+	}
+
+    for(i = 0; i < high; i++){
+	if((data+i)->key>=0){
+        *(TEMP+m) = *(data+i);
+	m++;
+		}
+	}
+    for(i = 0; i < high; i++){
+        *(data+i) = *(TEMP+i);
+	}
+
+
+        
+ 
+
+} 
+
+/**************************MERGE SORT*****************/
+void pSort::merge(dataType *data, int low, int center, int high)
+{
+//arr = [1,2,3,4,5,6,7,6,5,4,3,2,1]
+    int l1,l2,i,j,k;
+    l1 = center - low+1;
+    l2 = high  - center;
+   
+    pSort::dataType *LEFT = new pSort::dataType[l1]; 
+    pSort::dataType *RIGHT = new pSort::dataType[l2]; 
+    
+    for(int i = 0; i < l1; i++)
+        *(LEFT+i) = *(data+low + i);
+   
+        
+    for(int j = 0; j < l2; j++)
+        *(RIGHT+j) = *(data+center + j+1);
+
+    i = 0;  
+    j = 0;     
+    k = low;
+     
+
+    while (i < l1 && j < l2)
+    {
+        if ((LEFT+i)->key <= (RIGHT+j)->key) 
+        {
+            *(data+k)=*(LEFT+i);
+            i++;
+        }
+        else
+        {
+            *(data+k)=*(RIGHT+j);
+            j++;
+        }
+        k++;
+    }
+
+    while (i < l1) 
+    {
+        *(data+k)=*(LEFT+i);
+        i++;
+        k++;
+    }
+
+    while (j < l2)
+    {
+        *(data+k)=*(RIGHT+j);
+        j++;
+        k++;
+    }
+
+}
+
+void  pSort::merge_sort(dataType *data, int low, int high)
+{
+//arr = [2];
+// arr1 = [1,3,5,5] arr2= [2,4,6,8]
+//arr= [1,2,3,4,5,5,6,8]
+    if (low < high)
+    {        
+       
+        int m = low + (high - low) / 2;
+ 
+       
+        merge_sort(data, low, m);
+        merge_sort(data, m + 1, high);
+ 
+        merge(data, low, m, high);
+    }
+}
+
+/*************************BEST SORT*******************/
+void pSort::heapify(dataType *data, int n, int i) 
+{ 
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+  
+
+    if (l < n && (data+l)->key > (data+largest)->key) 
+        largest = l; 
+  
+
+    if (r < n && (data+r)->key > (data+largest)->key) 
+        largest = r; 
+  
+
+    if (largest != i) { 
+        swap(data+i, (data+largest)); 
+
+        heapify(data, n, largest); 
+    } 
+} 
+
+void pSort::heap_sort(dataType *data, int n) 
+{ 
+    int i;
+    for ( i = n / 2 - 1; i>=0; i--){
+        if(i >= 0)
+        break;
+        heapify(data, n, i);
+    } 
+         
+  
+    
+    for (int i = n - 1; i >= 0; i--) {        
+        swap(data, data+i);        
+        heapify(data, i, 0); 
+    } 
+}
+
+void pSort::Rearrange(dataType *data, int ndata, int size, int index, int offset){
+int length= ndata/size+2;
+pSort::dataType *Temp = new pSort::dataType[length];
+int k,j,i=0;
+for (i=0;i<length;i++){
+     *(Temp+i) = *(data + index+ i);  
+ }
+
+ i=0;
+ while(offset){
+     *(data+i)= *(Temp);
+     i=i+1;
+     offset=offset-1;
+}
+if(i!=0){
+    k=1;
+}else{
+    k=0;
+}
+
+ while(i<ndata){
+     j=0;
+     for(j=0;j<size && i<ndata; j++){
+         *(data+i)= *(Temp+k);
+         i++;
+     }
+     k=k+1;   
+ }
+
+}
+
+void pSort::init()
+   {   
+   MPI_Init(NULL, NULL);
+   }
+void pSort::print(dataType *data, int low, int high)
+{
+      for (int i=low;i<high;i++)
+      {
+        cout<<(data+i)->key<<endl;
+      }
+
+}
+void pSort::close()
+   {
+   MPI_Finalize();
+   }
+void pSort::sort(dataType *data, int ndata, SortType sorter)
+{
+   // printf("printing data in sort");
+   // printf(" ndata is %d    ", ndata);
+   // cout<< (data+1)->key;
+   int rank, size, lower, upper,n_record,i,index, offset,count;
+   MPI_Comm_size(MPI_COMM_WORLD, &size);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+   n_record=ndata/size;
+//    if (rank==0){
+//    cout<<"size "<<size<<" ndata "<<ndata<<" n_record "<<n_record<<endl;   
+//    }
+
+//    lower=n_record*rank;
+//    upper=lower+n_record;  
+//    if(rank==size-1)
+//    {     
+//       upper=ndata;
+//    }
+   
+   lower=0;
+   upper=ndata;
+
+   index=-1;
+   offset=0;
+   count=0;
+   //ndata=9;
+   while(count<=ndata*rank){
+       //cout<<"count in while is "<<count<<endl;
+       count=count+size;
+       index=index+1;
+   }
+   if ((ndata*rank)%count==0 && rank!=0){
+       //cout<<"count is equal to size*rank"<<count<<endl;
+       index=index-1;
+   }
+   //cout<<"count is "<<count<<endl;
+   while(count!=ndata*rank){
+      // cout<<"offset is getting changed "<<count<<" "<<ndata*rank <<endl;
+       count=count-1;
+       offset=offset+1;
+   }
+   if (offset==size){
+       offset=0;
+   }
+
+//cout<<"rank is "<<rank<<" index is: "<<index<<" offset is: "<<offset<<" count is: "<<count<<endl;
+// call a function according to offset and index
+
+
+
+
+ //  if (rank==0)
+ {
+      //cout<<"rank "<<rank <<" lower "<<lower<<" upper "<<upper<<endl;
+
+     
+        //printing
+        //print(data,lower,upper);
+        switch(sorter)
+        {
+        case BEST:
+        //cout<<"BEST is selected"<<endl;
+        heap_sort(data+lower, upper-lower); 
+        break;
+        case QUICK:
+        //cout<<"QUIK is selected"<<endl;
+        quick_sort(data,lower,upper-1);
+        break;
+        case MERGE:
+        //cout<<"MERGE is selected"<<endl;
+        merge_sort(data,lower,upper-1);
+        break;
+        case RADIX:
+        //cout<<"RADIX is selected"<<endl;
+        radix_sort(data,lower,upper);
+        break;
+        default:
+        cout<<"no valid choise about type of sort function"<<endl;
+        }
+ 
+      
+        //cout<<"print after sorting"<<endl;
+        //Rearrange(data,  ndata,  size,  index,  offset);
+//if (rank==0)
+       // print(data,lower,upper);
+      
+   }
+
+
+
+}
